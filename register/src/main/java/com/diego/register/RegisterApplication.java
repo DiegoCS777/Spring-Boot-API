d6ff27d7ef991c2b8c1bd8e3f4f52efc53253aa9
DiegoCS777 <diegomethal@gmail.com>
2022-05-22 15:58:48 -0300

    Creates UserRepository and CommandLineRunner in main class

--- register/src/main/java/com/diego/register/RegisterApplication.java
+++ register/src/main/java/com/diego/register/RegisterApplication.java
@@ -1,13 +1,33 @@
 package com.diego.register;
 
+import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class RegisterApplication {
+import com.diego.register.entities.User;
+import com.diego.register.repositories.UserRepository;
 
+@SpringBootApplication
+public class RegisterApplication implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RegisterApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		User u1 = new User(1L, "Igor", Instant.parse("2022-01-05T10:35:21Z"), Instant.parse("2022-07-05T10:41:21Z"), "65.256.658/0001-57", "oi");
+		User u2 = new User(2L, "José", Instant.parse("2022-03-05T11:35:21Z"), Instant.parse("2022-06-05T12:41:21Z"), "45.257.895/0001-32", "dae");
+		
+		userRepository.save(u1);
+		userRepository.save(u2);
 	}
 
 }
--- register/src/main/java/com/diego/register/resources/UserResource.java
+++ register/src/main/java/com/diego/register/resources/UserResource.java
@@ -1,9 +1,9 @@
 package com.diego.register.resources;
 
-import java.time.Instant;
-import java.util.ArrayList;
+
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,23 +11,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.diego.register.entities.User;
+import com.diego.register.repositories.UserRepository;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserRepository userRepository;
+	
 	@GetMapping
 	public ResponseEntity<List<User>> findAll(){
-		List<User> list = new ArrayList<>();
-		list.
-		add(new User(1L, "Igor", Instant.parse("2022-05-01T12:05:20Z"), Instant.parse("2022-05-01T12:05:20Z"), "025.625.894/0001-25", "oi" ));
+		List<User> list = userRepository.findAll();
 		
 		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
-		User u = new User(1L, "Igor", Instant.parse("2022-05-01T12:05:20Z"),Instant.parse("2022-05-01T12:05:20Z"),"025.625.894/0001-25", "oi");
+		User u = userRepository.findById(id);
 		return ResponseEntity.ok().body(u);
 	} 
 }
